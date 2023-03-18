9095520b26274a39ec0bec84f72036271b010e6e
Oradimi <loganargouse@gmail.com>
2023-03-18 13:53:55 +0100

    Add much more progress

--- joy_division/sketch.js
+++ joy_division/sketch.js
@@ -20,15 +20,7 @@ Pour n allant de 0 à nombre_de_lignes
 Fin Pour
 */
 
-const lineCount = 20;
-const quality = 4; // positive int. the lower, the better, but the more performance heavy
-const noiseScale = 0.02;
-const strokeW = 3;
-const micPower = 1000;
-
-let waveMin = 4;
-let waveMax;
-let randomTweak;
+// INITIAL SETUP //
 let mic;
 
 function preload() {
@@ -36,22 +28,33 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1000, 1200); //width/2 = 512.55
-  frameRate(24);
+  createCanvas(750, 900);
+  frameRate(24); // affects the maximum framerate
   randomSeed(Date.now());
 
   mic = new p5.AudioIn();
   mic.start();
 }
 
-const lineLength = 1000 / 6;
-const transitionTime = 1000 / 6;
+// CONSTANT VALUES //
+const noiseScale = 0.01; // the higher, the more spiky it gets
+const micPower = 1000; // the higher, the more mic input influences the lines
+const lineCount = 80; // the more lines, the slower
+const strokeWeightValue = 3; // the higher, the thicker the lines gets
+const quality = 6; // the lower, the better, but the more performance heavy
 
 function draw() {
   background(0);
   
   console.log(frameRate());
 
+  // FRAME RESET VALUES //
+  let lineLength = width * 0.15;
+  let transitionTime = width * 0.15;
+  let waveMin = 12;
+  let waveMax;
+  let randomTweak;
+  let duringTransition;
   let margin = height / 10;
   let marginSides = height / 10;
   let lineDistance = (height - margin * 2) / (lineCount + 1);
@@ -59,58 +62,59 @@ function draw() {
   let endChange = (width / 2) + (lineLength / 2);
   let verticalShift = margin * 1.4;
   let waveValue = 0;
+  let noiseValue = 0;
+  let verticalShiftNoise = 0;
 
   for(let j = 0; j < lineCount; j++) {
-    //waveMax = mic.getLevel() * micPower;
-    waveMax = 100;
+    // LINE RESET VALUES //
+    waveMax = mic.getLevel() * micPower;
     randomTweak = random() * 100;
     verticalShift = verticalShift + lineDistance;
-    let verticalShiftNoiseList = new Array();
-    let noiseValue = 0;
-    let verticalShiftNoise = 0;
+    noiseValue = 0;
+    verticalShiftNoise = 0;
+    waveValue = waveMin;
 
+    // WAVES //
     stroke(255);
     fill(0);
     beginShape(TESS);
     vertex(marginSides / 2, height * 1.1);
-    vertex(marginSides / 2, verticalShift);
+    vertex(marginSides / 2, verticalShift - waveMin);
     let y = 0;
     for (let x = marginSides; x < width - marginSides; x += quality) {
-      let adjust = waveMax * Math.sin(y * Math.PI * quality / transitionTime);
-      if (x > startChange && x < endChange) {
+      duringTransition = x >= startChange - transitionTime && x <= startChange
+        || x >= endChange && x <= endChange + transitionTime;
+      inMiddle = x > startChange && x < endChange;
+      if (duringTransition && waveMin < waveMax) {
+        waveValue = waveMin + abs(waveMax - waveMin) * (1 - Math.cos(Math.PI * y / transitionTime)) / 2;
+        y += quality;
+      }
+      else if (inMiddle) {
         waveValue = max(waveMax, waveMin);
       }
-      else if (x > startChange - transitionTime && x < startChange) {
-        if (waveValue < waveMax) {
-          waveValue = waveValue + adjust;
-          y++;
-        }
-      }
-      else if (x > endChange && x <= endChange + transitionTime) {
-        if (waveValue > waveMin) {
-          waveValue = waveValue - adjust;
-          y--;
-        }
-      }
-      else if (x < startChange || x > endChange) {
-        waveValue = waveMin;
-      }
+      else waveValue = waveMin;
       noiseValue = noise(x * noiseScale, randomTweak * (j + 1) * noiseScale);
-      verticalShiftNoise = - noiseValue * waveValue;
-      verticalShiftNoiseList.push(verticalShiftNoise);
+      noiseValueSecond = noise(x * noiseScale / 2, randomTweak * (j + 1) * noiseScale / 2);
+      verticalShiftNoise = (- waveValue) * noiseValue * noiseValueSecond;
       
       stroke(255);
-      strokeWeight(strokeW);
+      strokeWeight(strokeWeightValue);
       vertex(x, verticalShift + verticalShiftNoise);
     }
-    vertex(width - marginSides / 2, verticalShift);
+    vertex(width - marginSides / 2, verticalShift - waveMin);
     vertex(width - marginSides / 2, height * 1.1);
     endShape(CLOSE);
   }
+
+  // FRAME //
   fill(0);
   noStroke();
   rect(0, 0, marginSides, height);
   rect(width, 0, -marginSides, height);
+  fill(0, 0, 0, 100);
+  rect(0, 0, width, height * 0.12);
+
+  // TEXT //
   textAlign(CENTER);
   textFont(univers);
   fill(255);
